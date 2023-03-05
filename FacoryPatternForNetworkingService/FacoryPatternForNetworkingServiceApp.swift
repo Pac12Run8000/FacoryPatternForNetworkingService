88e9c08231ea4f13247f599f43086e995d4c38db
Pac12Run8000 <gangstarr78992@>
2023-03-05 07:06:05 -0800

    Initial Commit

---
+++ FacoryPatternForNetworkingService/FacoryPatternForNetworkingServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacoryPatternForNetworkingServiceApp.swift
+//  FacoryPatternForNetworkingService
+//
+//  Created by Michelle Grover on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct FacoryPatternForNetworkingServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
